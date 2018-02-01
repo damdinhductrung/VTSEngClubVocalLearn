bf6d124fe24aa15e6f2d397049a8d0ac00cca6af
Trung Linux <damdinhductrung@gmail.com>
2018-02-01 09:18:03 +0700

    update sql

--- Resources/grade.sql
+++ Resources/grade.sql
@@ -4,7 +4,7 @@ LIMIT 0, 1000
 
 -- Date: 2018-01-20 09:56
 */
-INSERT INTO `grade` (`SEQ`,`Number`) VALUES (1,6);
-INSERT INTO `grade` (`SEQ`,`Number`) VALUES (2,7);
-INSERT INTO `grade` (`SEQ`,`Number`) VALUES (3,8);
-INSERT INTO `grade` (`SEQ`,`Number`) VALUES (4,9);
+INSERT INTO `Grade` (`SEQ`,`Number`) VALUES (1,6);
+INSERT INTO `Grade` (`SEQ`,`Number`) VALUES (2,7);
+INSERT INTO `Grade` (`SEQ`,`Number`) VALUES (3,8);
+INSERT INTO `Grade` (`SEQ`,`Number`) VALUES (4,9);
